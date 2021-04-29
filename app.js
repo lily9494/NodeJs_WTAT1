ecbaeb35adf0beb276bb7fc781f8dde92599caad
lily9494 <ahmadi.nilufar12@gmail.com>
2021-04-29 11:34:44 +0200

    last change

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+var routeResponseMap={
+    "/info":"<h1>Info Page</h1>"
+}
 var http=require('http');
 var fs=require('fs');
 var moduleN=require('./moduleN');
+const getJSONString = obj => {
+    return JSON.stringify(obj, null, 2);
+   };
 function onRequest(request,response){
+    if(routeResponseMap[request.url]){
+        response.end(routeResponseMap[request.url])
+    }
+    var body=[];
+    request.on('data',(bodyData)=>{
+             body.push(bodyData);
+    });
     response.writeHead(200,{'content-type':'text/html'})
     fs.readFile('./index.html',null,function(error,data){
         if(error){
@@ -11,6 +24,7 @@ function onRequest(request,response){
             response.write(data);
         }
         response.end()
+        console.log(`Method: ${getJSONString(request.method)}`);
     });
     
 }
